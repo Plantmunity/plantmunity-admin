135a5043c3bce024b8c43bdcb73afc388ec55c55
ThomasRhodz <J.rodis.477524@umindanao.edu.ph>
2023-06-06 21:22:35 +0800

    with manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,26 @@
 module.exports = {
   siteMetadata: {
     title: `Plantmunity Admin Frontend`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-top-layout",
-    "gatsby-plugin-image", {
-      resolve: 'gatsby-plugin-manifest',
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-manifest",
       options: {
-        "icon": "src/images/icon.png"
-      }
-    }, 
-    "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "images",
-        "path": "./src/images/"
+        icon: "src/images/icon.png",
       },
-      __key: "images"
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
@@ -26,10 +29,22 @@ module.exports = {
           `limelight`,
           `Arvo`,
           `Raleway`,
-          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
         ],
-        display: 'swap'
-      }
-    }
-  ]
+        display: "swap",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: `Plantmunity`,
+        short_name: `Plantmunity`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#5C6D63`,
+        display: `standalone`,
+        icon: "src/images/PlantmunityIconAlt.png",
+      },
+    },
+  ],
 };
