8422f60390944624f9ea9b7cf1c60a5d6271e147
John Eliezar Rodis <J.rodis.477524@umindanao.edu.ph>
2023-12-08 20:12:03 +0800

    very minor fix

--- src/components/basic/StyledComponents.js
+++ src/components/basic/StyledComponents.js
@@ -2,45 +2,46 @@ import { styled } from "@mui/material/styles";
 import { TextField } from "@mui/material";
 
 export const SearchField = styled(TextField)(() => ({
-  backgroundColor:'transparent',
-  width:'100%',
-  '& label.Mui-focused': {
-    color: 'transparent',
+  backgroundColor: "transparent",
+  width: "100%",
+  "& label.Mui-focused": {
+    color: "transparent",
   },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: 'transparent',
-  },  
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'transparent',
+
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "transparent",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "transparent",
     },
-    '&:hover fieldset': {
-      borderColor: 'transparent',
+    "&:hover fieldset": {
+      borderColor: "transparent",
     },
-    '&.Mui-focused fieldset': {
-      borderColor: 'transparent',
+    "&.Mui-focused fieldset": {
+      borderColor: "transparent",
     },
-},
+  },
 }));
 
 export const TextArea = styled(TextField)(() => ({
-  backgroundColor:'transparent',
-  width:'100%',
-  '& label.Mui-focused': {
-    color: 'transparent',
+  backgroundColor: "transparent",
+  width: "100%",
+  "& label.Mui-focused": {
+    color: "transparent",
   },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: 'transparent',
-  },  
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'transparent',
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "transparent",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "transparent",
     },
-    '&:hover fieldset': {
-      borderColor: 'transparent',
+    "&:hover fieldset": {
+      borderColor: "transparent",
     },
-    '&.Mui-focused fieldset': {
-      borderColor: 'transparent',
+    "&.Mui-focused fieldset": {
+      borderColor: "transparent",
     },
-},
+  },
 }));
